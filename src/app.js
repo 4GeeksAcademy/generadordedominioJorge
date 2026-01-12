f39b5f0d0fd0c8c12192160e0d805d3c988a5c5b
JorgeS263 <jorgee.serraano@gmail.com>
2026-01-12 20:54:57 +0000

    generadorhecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+    let pronombre = ["el", "la", "las"];
+    let adjetivo = ["deporte", "increible", "salsa"];
+    let nombre = ["pelotas", "tijeras", "ramas"];
+    let dominio = [".es", ".com", ".zapatilla"];
+
+
+
+  for (let p = 0; p < pronombre.length; p++) {
+    for (let a = 0; a < adjetivo.length; a++) {
+      for (let n = 0; n < nombre.length; n++) {
+        for (let d = 0; d < dominio.length; d++) {
+
+          console.log(pronombre[p]+ adjetivo[a]+ nombre[n]+ dominio[d]
+          );
+
+        }
+      }
+    }
+  }
 };
+
